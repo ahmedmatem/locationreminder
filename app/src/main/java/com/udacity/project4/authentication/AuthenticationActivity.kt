1b7ca2ee5edad6bb980f5103c23038d0e3406dbb
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-06-12 14:14:42 +0100

    Implement sign in onActivityResult

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,12 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import kotlinx.android.synthetic.main.activity_authentication.*
 
@@ -28,6 +32,19 @@ class AuthenticationActivity : AppCompatActivity() {
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                Log.d(TAG, "onActivityResult: Successfully signed in user ${user?.displayName}")
+            } else {
+                Log.d(TAG, "Sign in unsuccessful: ${response?.error?.errorCode}")
+            }
+        }
+    }
+
     private fun launchSignInFlow() {
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
@@ -44,6 +61,7 @@ class AuthenticationActivity : AppCompatActivity() {
     }
 
     companion object {
+        const val TAG = "AuthenticationActivity"
         private const val SIGN_IN_REQUEST_CODE = 722
     }
 }
