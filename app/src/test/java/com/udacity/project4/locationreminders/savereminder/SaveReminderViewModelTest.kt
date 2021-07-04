b688844ca834e78df51226128771302b56dbb64f
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-07-04 08:57:29 +0100

    Implement Fake data source or doubble local  db

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,8 +1,14 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.ReminderDataSource
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -12,5 +18,21 @@ class SaveReminderViewModelTest {
 
     //TODO: provide testing to the SaveReminderView and its live data objects
 
+    // Subject under test
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupViewModel() {
+//        saveReminderViewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(), )
+
+    }
+
+    @Test
+    fun reminderTitle_setTitle() {
+        // GIVEN a fresh ViewModel
+
+    }
 }
