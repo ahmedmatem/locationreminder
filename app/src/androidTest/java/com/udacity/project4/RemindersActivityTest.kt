7d14322b67773a710fcf16fcc87aa0387d67e994
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-07-26 07:03:09 +0100

    Implement end2end test

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,14 +1,21 @@
 package com.udacity.project4
 
 import android.app.Application
+import android.app.Instrumentation
+import android.view.View
+import android.widget.TextView
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -21,15 +28,20 @@ import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.RecyclerViewItemCountAssertion
+import com.udacity.project4.util.monitorActivity
 import com.udacity.project4.utils.EspressoIdlingResource
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers.hasEntry
 import org.hamcrest.core.Is.`is`
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -46,7 +58,17 @@ class RemindersActivityTest :
     AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
 
     private lateinit var repository: ReminderDataSource
-    private lateinit var appContext: Application
+    private lateinit var viewModel: SaveReminderViewModel
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+//    private lateinit var appContext: Application
+
+    /**
+     * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
+     * at this step we will initialize Koin related code to be able to use it in out testing.
+     */
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -54,28 +76,6 @@ class RemindersActivityTest :
      */
     @Before
     fun init() {
-//        stopKoin()//stop the original app koin
-//        appContext = getApplicationContext()
-//        val myModule = module {
-//            viewModel {
-//                RemindersListViewModel(
-//                    appContext,
-//                    get() as ReminderDataSource
-//                )
-//            }
-//            single {
-//                SaveReminderViewModel(
-//                    appContext,
-//                    get() as ReminderDataSource
-//                )
-//            }
-//            single { RemindersLocalRepository(get()) as ReminderDataSource }
-//            single { LocalDB.createRemindersDao(appContext) }
-//        }
-//        //declare a new koin module
-//        startKoin {
-//            modules(listOf(myModule))
-//        }
         //Get our real repository
         repository = get()
 
@@ -112,19 +112,26 @@ class RemindersActivityTest :
         )
 
         val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.remindersRecyclerView)).check(RecyclerViewItemCountAssertion(0))
 
         // click on the add reminder fab
         onView(withId(R.id.addReminderFAB)).perform(click())
         onView(withId(R.id.reminderTitle)).perform(replaceText(reminderDto.title))
         onView(withId(R.id.reminderDescription)).perform(replaceText(reminderDto.description))
-//        onView(withId(R.id.selectedLocation)).perform(replaceText("location_1"))
 
-        // click on save fab button to add reminder
+        onView(withId(R.id.selectLocation)).perform(click())
+
+        // Pause google map fragment for a while to be able to select a POI
+        Thread.sleep(5000)
+
+        onView(withId(R.id.save_location_btn)).perform(click())
+
         onView(withId(R.id.saveReminder)).perform(click())
 
-        // verify that reminder displayed on screen
-        onData(allOf(`is`(instanceOf(Map::class.java)), `is`(withText(reminderDto.title))))
-            .check(matches(isDisplayed()))
+        // List items count must be 1
+        onView(withId(R.id.remindersRecyclerView)).check(RecyclerViewItemCountAssertion(1))
 
         activityScenario.close()
     }
